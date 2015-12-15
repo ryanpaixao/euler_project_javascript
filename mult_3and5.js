dcb66fdda3a0be3b1dfb18f9d00b565c7010eb1d
ryanpaixao <ryanpaixao@gmail.com>
2015-12-14 19:21:24 -0500

    Simplify javascript

--- mult_3and5.js
+++ mult_3and5.js
@@ -1,28 +1,13 @@
-var answers = [];
 var ans_total = 0;
 
 var num_sort = function ()	{
 	for (var i = 1; i < 1000; i++)	{
-		if ((i % 3 === 0)||(i % 5 ===0))	{
-			answers.push(i);
-		}
-		else	{
-
-		}
+		if ((i % 3 === 0)||(i % 5 === 0))	{
+			ans_total += i;
+		};
 	};
 };	
 
-var sum_sort = function (answers)	{
-	for (var j = 0; j < answers.length; j++)	{
-		ans_total += answers[j];
-	}
-};
-
 num_sort();
-sum_sort(answers);
+document.write(ans_total);
 
-document.write('<div><p>This JavaScript program answers the following question: <br/><br/><br/></p></div>');
-
-document.write('<div><p>If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.</p></div>');
-document.write('<div><p><br/><br/>The answer is: </p></div>');
-document.write('<div><p>' + ans_total + '</p></div>');
