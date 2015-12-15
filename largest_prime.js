1361802a00e13d01de6aa6704574f735423504d1
ryanpaixao <ryanpaixao@gmail.com>
2015-12-15 00:51:44 -0500

    Update largest_prime. Needs to be tested.

--- largest_prime.js
+++ largest_prime.js
@@ -1,10 +1,10 @@
 var prime_stat = false;
+var factors = [];
+var largest_factor = 0;
 
 var is_prime = function (number)	{
 	for (var i = 2; i < number; i++)	{
-	    document.write(i);
 		if ((number % i) === 0) {
-			document.write("no")
 			prime_stat = false;
 			break;
 		}
@@ -12,7 +12,31 @@ var is_prime = function (number)	{
 			prime_stat = true;
 		};
 	};
+	if (prime_stat)	{
+		largest_factor = number;
+	}
 };
 
-is_prime(99);
-document.write(prime_stat);
+
+var find_factors = function (number)	{
+	for (var i = 2; i < number; i++)	{
+		if ((number % i) === 0) {
+			factors.push(i);
+		}
+		else	{
+			
+		};
+	};
+};
+
+var check_factors = function ()	{
+	for (var i = 0; i < factors.length; i++)	{
+		is_prime(factors[i]);
+	}
+}
+
+
+find_factors(600851475143);
+check_factors();
+document.write(factors +"<br/><br/>");
+document.write(largest_factor);
